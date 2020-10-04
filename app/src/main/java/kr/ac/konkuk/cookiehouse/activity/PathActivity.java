f67fe2186a3a0f6463f647a07c8f9d525a79b644
chaneeii <chan420420@gmail.com>
2020-10-04 09:39:18 +0900

    1. bottom-nav : styled, icon effects, material design, intent change 2. toolbar : center-aligned title, menu included 3. new font & renamed icons

---
+++ app/src/main/java/kr/ac/konkuk/cookiehouse/activity/PathActivity.java
@@ -0,0 +1,50 @@
+package kr.ac.konkuk.cookiehouse.activity;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import kr.ac.konkuk.cookiehouse.R;
+import kr.ac.konkuk.cookiehouse.Utils.BottomNavigationViewHelper;
+
+public class PathActivity extends AppCompatActivity {
+    private static final String TAG = "PathActivity";
+    private static final int ACTIVITY_NUM=1;
+    private Context mContext = PathActivity.this;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Log.d(TAG, "onCreate : started");
+
+
+
+        setupBottomNavigationView();
+    }
+
+
+    /*
+     * Bottom Navigation view setup
+     * */
+    private void setupBottomNavigationView(){
+        Log.d(TAG,"SetupBottomNavigationView : setting up BottomNavigationView");
+        BottomNavigationView bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottom_navigation);
+        BottomNavigationViewHelper.enableNavigation(mContext, bottomNavigationView);
+        Menu menu = bottomNavigationView.getMenu();
+        MenuItem menuItem = menu.getItem(ACTIVITY_NUM);
+        menuItem.setChecked(true);
+
+    }
+
+
+}
