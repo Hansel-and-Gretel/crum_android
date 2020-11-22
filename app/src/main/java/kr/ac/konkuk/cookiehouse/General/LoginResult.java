2942953d568c9d2361ffbd344ce76e5e4c63d2d3
chaneeii <chan420420@gmail.com>
2020-11-22 16:07:56 +0900

    backup for merging with places DB

---
+++ app/src/main/java/kr/ac/konkuk/cookiehouse/General/LoginResult.java
@@ -0,0 +1,22 @@
+package kr.ac.konkuk.cookiehouse.General;
+
+import com.google.gson.annotations.SerializedName;
+
+public class LoginResult {
+
+    private String name;
+    private String email;
+    private String lifeStyle;
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getLifeStyle() {
+        return lifeStyle;
+    }
+}
---
+++ app/src/main/java/kr/ac/konkuk/cookiehouse/General/RetrofitInterface.java
@@ -0,0 +1,31 @@
+package kr.ac.konkuk.cookiehouse.General;
+
+import java.util.HashMap;
+
+import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.POST;
+
+public interface RetrofitInterface {
+
+    @POST("/login")
+    Call<LoginResult> executeLogin(@Body HashMap<String, String> map);
+
+    /*
+    * Call<객체타입>
+    *
+    * @GET("/login")
+    * 이렇게 하는법도 잇음
+    Call<LoginResult> executeLogin(@Query("email") String email,@Query("pw") String pw) ;
+    * *
+
+    Get 방식일때 @Query 와 @QueryMap 을 사용한다.
+    Post 방식일때 @Field 와 @FieldMap 을 사용한다.
+    Interceptor 개념은 OkHttpClient 라이브러리가 적용된 프로젝트에만 국한된 개념이다.
+    Interceptor 는 크게 Applicaion 과 Network 로 나눌수 있다.
+     */
+
+    @POST("/signup")
+    Call<Void> executeSignup(@Body HashMap<String, String> map);
+
+}
