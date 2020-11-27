0b82de3b30ca833f85c6653436664ace9d11ac9f
chaneeii <chan420420@gmail.com>
2020-11-28 00:27:20 +0900

    Change Register New user - server api changed

--- app/src/main/java/kr/ac/konkuk/cookiehouse/General/RetrofitInterface.java
+++ app/src/main/java/kr/ac/konkuk/cookiehouse/General/RetrofitInterface.java
@@ -10,8 +10,22 @@ import retrofit2.Call;
 import retrofit2.http.Body;
 import retrofit2.http.GET;
 import retrofit2.http.POST;
+/*
+ * Call<객체타입>
+ *
+ * @GET("/login")
+ * 이렇게 하는법도 잇음
+ Call<LoginResult> executeLogin(@Query("email") String email,@Query("pw") String pw) ;
+ * *
+
+ Get 방식일때 @Query 와 @QueryMap 을 사용한다.
+ Post 방식일때 @Field 와 @FieldMap 을 사용한다.
+ Interceptor 개념은 OkHttpClient 라이브러리가 적용된 프로젝트에만 국한된 개념이다.
+ Interceptor 는 크게 Applicaion 과 Network 로 나눌수 있다.
+  */
 
 public interface RetrofitInterface {
+
 
     @POST("/api/user/login")
     Call<ModelUser> executeLogin(@Body HashMap<String, String> map);
@@ -19,23 +33,10 @@ public interface RetrofitInterface {
     @GET("/api/user/auth")
     Call<ModelUser> getUserInfo();
 
-    /*
-    * Call<객체타입>
-    *
-    * @GET("/login")
-    * 이렇게 하는법도 잇음
-    Call<LoginResult> executeLogin(@Query("email") String email,@Query("pw") String pw) ;
-    * *
-
-    Get 방식일때 @Query 와 @QueryMap 을 사용한다.
-    Post 방식일때 @Field 와 @FieldMap 을 사용한다.
-    Interceptor 개념은 OkHttpClient 라이브러리가 적용된 프로젝트에만 국한된 개념이다.
-    Interceptor 는 크게 Applicaion 과 Network 로 나눌수 있다.
-     */
-
-    @POST("/api/user/signup")
+    @POST("/api/user/register")
     Call<Void> executeSignup(@Body HashMap<String, String> map);
 
     @POST("/api/journey/upload")
     Call<ModelJourney> createJourney(@Body JSONObject newJourney);
+
 }
